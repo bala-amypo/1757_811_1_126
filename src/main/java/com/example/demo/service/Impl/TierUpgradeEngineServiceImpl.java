5ff44a062da1ae86c36cfc38e71ac752ed090e54
bala-amypo <bala@amypo.in>
2025-12-18 08:49:52 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TierUpgradeEngineServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TierUpgradeEngineServiceImpl.java
@@ -13,7 +13,11 @@ public class TierUpgradeEngineServiceImpl implements TierUpgradeEngineService {
     private final TierUpgradeRuleRepository ruleRepo;
     private final TierHistoryRecordRepository historyRepo;
 
-    public TierUpgradeEngineServiceImpl(CustomerProfileRepository c,PurchaseRecordRepository p,VisitRecordRepository v,TierUpgradeRuleRepository r,TierHistoryRecordRepository h) {
+    public TierUpgradeEngineServiceImpl(CustomerProfileRepository c,
+                                        PurchaseRecordRepository p,
+                                        VisitRecordRepository v,
+                                        TierUpgradeRuleRepository r,
+                                        TierHistoryRecordRepository h) {
         this.customerRepo = c;
         this.purchaseRepo = p;
         this.visitRepo = v;
