f62a9c8fae36254399b9497eaf5542a0cd5e202d
bala-amypo <bala@amypo.in>
2025-12-18 07:43:14 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/CustomerProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/CustomerProfileServiceImpl.java
@@ -1,9 +1,43 @@
-package com.example.demo.repository;
+package com.example.demo.service.impl;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
+import com.example.demo.service.*;
+import com.example.demo.repository.*;
 import com.example.demo.model.*;
+import java.util.*;
 
-public interface CustomerProfileRepository extends JpaRepository<CustomerProfileEntity, Long> {
-    Optional<CustomerProfileEntity> findByCustomerId(String customerId);
+public class CustomerProfileServiceImpl implements CustomerProfileService {
+
+    private final CustomerProfileRepository repo;
+
+    public CustomerProfileServiceImpl(CustomerProfileRepository repo) {
+        this.repo = repo;
+    }
+
+    public CustomerProfileEntity createCustomer(CustomerProfileEntity c) {
+        return repo.save(c);
+    }
+
+    public CustomerProfileEntity getCustomerById(Long id) {
+        return repo.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    public CustomerProfileEntity findByCustomerId(String customerId) {
+        return repo.findByCustomerId(customerId).orElseThrow(NoSuchElementException::new);
+    }
+
+    public List<CustomerProfileEntity> getAllCustomers() {
+        return repo.findAll();
+    }
+
+    public CustomerProfileEntity updateTier(Long id, String tier) {
+        CustomerProfileEntity c = getCustomerById(id);
+        c.setCurrentTier(tier);
+        return repo.save(c);
+    }
+
+    public CustomerProfileEntity updateStatus(Long id, boolean active) {
+        CustomerProfileEntity c = getCustomerById(id);
+        c.setActive(active);
+        return repo.save(c);
+    }
 }
