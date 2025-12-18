e198b74c917a2567f57ba42859a080df9e1409f0
bala-amypo <bala@amypo.in>
2025-12-18 07:51:49 +0000

    User commit

--- src/main/java/com/example/demo/service/VisitRecordService.java
+++ src/main/java/com/example/demo/service/VisitRecordService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import java.util.*;
 import com.example.demo.model.*;
+
 public interface VisitRecordService {
     VisitRecordEntity recordVisit(VisitRecordEntity visit);
     List<VisitRecordEntity> getVisitsByCustomer(Long id);
