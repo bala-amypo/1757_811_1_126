2abc54042d1c39a768dff52c879f11718b8ce1aa
bala-amypo <bala@amypo.in>
2025-12-19 04:20:04 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/CustomerProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/CustomerProfileServiceImpl.java
@@ -1,7 +1,8 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.model.CustomerProfileEntity;
 import com.example.demo.repository.CustomerProfileRepository;
+import com.example.demo.service.CustomerProfileService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
