17690311f40c183ae55c7635a7a7de3a8b1394a7
bala-amypo <bala@amypo.in>
2025-12-19 10:22:08 +0000

    User commit

--- src/main/java/com/example/demo/service/CustomerProfileService.java
+++ src/main/java/com/example/demo/service/CustomerProfileService.java
@@ -1,13 +1,24 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.CustomerProfile;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomerProfileService {
+
     CustomerProfile createCustomer(CustomerProfile customer);
+
     CustomerProfile getCustomerById(Long id);
+
     CustomerProfile findByCustomerId(String customerId);
+
     List<CustomerProfile> getAllCustomers();
+
     CustomerProfile updateTier(Long id, String newTier);
+
     CustomerProfile updateStatus(Long id, boolean active);
+
+ 
+    Optional<CustomerProfile> findByEmail(String email);
 }
