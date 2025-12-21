d044fa4bd884fbb47390d9d6afdc5b184f014dde
bala-amypo <bala@amypo.in>
2025-12-21 05:48:05 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,21 +1,39 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.CustomerProfile;
+import com.example.demo.service.CustomerProfileService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    @PostMapping("/register")
-    public String register(@RequestParam String fullName,
-                           @RequestParam String email,
-                           @RequestParam String password) {
-        return "User registered successfully: " + email;
+    private final CustomerProfileService customerProfileService;
+
+    public AuthController(CustomerProfileService customerProfileService) {
+        this.customerProfileService = customerProfileService;
     }
 
+
+    @PostMapping("/register")
+    public CustomerProfile register(@RequestBody CustomerProfile customerProfile) {
+        return customerProfileService.createCustomer(customerProfile);
+    }
+
+   
     @PostMapping("/login")
-    public String login(@RequestParam String email,
-                        @RequestParam String password) {
-        return "Login successful for: " + email;
+    public Map<String, String> login(@RequestParam String customerId) {
+        CustomerProfile customer =
+                customerProfileService.findByCustomerId(customerId);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Login successful");
+        response.put("customerId", customer.getCustomerId());
+        response.put("currentTier", customer.getCurrentTier());
+
+        return response;
     }
 }
