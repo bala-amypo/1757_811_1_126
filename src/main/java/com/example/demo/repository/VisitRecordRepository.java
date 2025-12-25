078a849d99dbbdf12369d54c754ed4f160902208
bala-amypo <bala@amypo.in>
2025-12-25 04:27:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/VisitRecordRepository.java
+++ src/main/java/com/example/demo/repository/VisitRecordRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.*;
+import com.example.demo.model.VisitRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.*;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
+import java.util.List;
 
+@Repository
 public interface VisitRecordRepository extends JpaRepository<VisitRecord, Long> {
+    @Query("SELECT v FROM VisitRecord v WHERE v.customer.id = :customerId")
     List<VisitRecord> findByCustomerId(Long customerId);
+    List<VisitRecord> findByVisitDateBetween(LocalDate start, LocalDate end);
 }
